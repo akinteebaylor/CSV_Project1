cd58647fc5e29121483869751b810d6f9f52c0cf
Asaolu <Akintoye_Asaolu1@baylor.edu>
2020-01-29 14:53:23 -0600

    Created 4th Plot and used death valley

--- csv_temps-4.py
+++ csv_temps-4.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 csv_file = csv.reader(open_file, delimiter=",")
 header_row =next(csv_file)
 for index, column_header in enumerate(header_row):
@@ -11,10 +11,16 @@ highs =[]
 dates =[]
 lows=[]
 for row in csv_file:
-    highs.append(int(row[5]))    
-    lows.append(int(row[6]))
-    current__date = datetime.strptime(row[2],'%Y-%m-%d')
-    dates.append(current__date)
+    try:
+        high = int(row[4])
+        low =int(row[5])
+        current__date = datetime.strptime(row[2],'%Y-%m-%d')
+    except ValueError:
+        print(f"missing data for {current__date}")
+    else:
+        highs.append(high) 
+        lows.append(low)
+        dates.append(current__date)
 print(highs[:10])
 print(lows[:10])
 
@@ -22,7 +28,7 @@ fig = plt.figure()
 plt.plot(dates,lows,color='blue',alpha=0.5)
 plt.plot(dates,highs,color='red', alpha=0.5)
 plt.fill_between(dates,highs,lows, facecolor='blue', alpha =0.5)
-plt.title ("daily high temps, July 2018", fontsize =16)
+plt.title ("daily high temps for death valley", fontsize =16)
 plt.xlabel ("", fontsize=10)
 plt.ylabel ("Temperature (F) ", fontsize =12)
 plt.tick_params(axis ='both', which="major", labelsize=12 )
